1d4c1cd19c32fb98c63a2342073aa313130b38e4
henricgustafsson <50734521+henricgustafsson@users.noreply.github.com>
2019-10-29 16:52:35 +0100

    luuuuuuuv2code

---
+++ src/main/java/main.java
@@ -0,0 +1,25 @@
+public class main {
+
+    public static void main(String[] args) {
+
+        //TODO: ta reda på hur det funkar med spring jars till intelliJ - VÄGRAR gå tbx till eclipse
+       //luv2code https://lexicon.udemy.com/course-dashboard-redirect/?course_id=1217894
+
+        //https://www.jetbrains.com/help/idea/spring-support.html
+
+        //luv2codes jar samling - OBS endast >= 5.2.0
+        //https://repo.spring.io/release/org/springframework/spring/5.2.0.RELEASE/
+
+        ////Spring Core -kan en använda dependency ist för jars?
+        //https://mvnrepository.com/artifact/org.springframework/spring-core
+
+        /*
+        <!-- https://mvnrepository.com/artifact/org.springframework/spring-context -->
+        <dependency>
+            <groupId>org.springframework</groupId>
+            <artifactId>spring-context</artifactId>
+            <version>5.1.3.RELEASE</version>
+        </dependency>
+        * */
+    }
+}
